0be945c6aa172758c9913633ec8c4723d7d9b1f5
JovanVrucina <novokmet.jovan14@gmail.com>
2023-05-14 19:50:40 +0200

    modified:   .gitignore 	new file:   neuralNet/version-RFB-640.onnx 	new file:   neuralNet/yolov5m.onnx

--- include/FaceBodyRecognition.hpp
+++ include/FaceBodyRecognition.hpp
@@ -5,21 +5,14 @@
 #ifndef FACE_AND_BODY_H
 #define FACE_AND_BODY_H
 
-/*CONFIGURATION BEGIN*/
-const int samplesTaken = 1;
-const int minConfidencePercentage = 100;
-constexpr int minConfirmed(){
-	return round((double)samplesTaken*((double)minConfidencePercentage/100));
-	}
 /*NEW CONFIGURATION*/
 const float INPUT_WIDTH = 640.0;
 const float INPUT_HEIGHT = 640.0;
 const float SCORE_THRESHOLD = 0.5;
 const float NMS_THRESHOLD = 0.45;
 const float CONFIDENCE_THRESHOLD = 0.45;
-/*END NEW CONFIG*/
+/*END NEW CONFIGURATION*/
 
-/*CONFIGURATION END*/
 bool isPresent(cv::CascadeClassifier Cascade, cv::Mat Frame, cv::Rect &destinationRectangle);
 bool isPresent(cv::CascadeClassifier Cascade, cv::Mat *stream, cv::Rect &destinationRectangle);
 
