89199fe39c2291d38f00188118a9b53f466564ce
Jovan Novokmet <novokmet.jovan14@gmail.com>
2023-09-29 14:27:57 +0200

    deleted:    cascades/haarcascade_frontalcatface.xml 	deleted:    cascades/haarcascade_upperbody.xml 	modified:   main.cpp 	modified:   projectCompile.sh Now works with raspberry pi

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include<opencv2/core.hpp>
-#include<opencv2/videoio.hpp>
-#include<opencv2/highgui.hpp>
+#include<opencv2/opencv.hpp>
 #include<iostream>
 #include<fstream>
 #include<future>
@@ -16,8 +14,15 @@ int main(int argc, char *argv[]){
 		cout<<"Error!"<<'\n';
 		return -1;
 	}
-	GPIO::DigitalOut bit1(02);
-	GPIO::DigitalOut bit2(03);
+	
+	GPIO::DigitalOut leftGPIO(16);
+	GPIO::DigitalOut rightGPIO(20);
+	GPIO::DigitalOut StandbyGPIO(21);
+
+	leftGPIO.off();
+	rightGPIO.off();
+	StandbyGPIO.off();
+
 	cv::Mat frame;
 	cv::Rect backRectangle; //rectangle representing the first detected back
 	cv::Rect faceRectangle; //rectangle representing the first detected face
@@ -41,31 +46,28 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 	cout<<"done"<<'\n'<<"ready."<<'\n';
-	cv::namedWindow("out", cv::WINDOW_AUTOSIZE);
 	//Start of the main code loop
 	while(true){
 		camera>>frame;
 		vector<cv::Mat> networkOutputsBody = pre_processYOLO(frame, bodyNetwork);
 		cv::Rect bodyRectangle = post_processYOLO(frame, networkOutputsBody);
 		
-		if(bodyRectangle.x >= 0){
-			cout<<bodyRectangle.x+(bodyRectangle.width/2)<<'\n';
-			bit1.off();
-			bit2.off();
-		}else{
-			int midpoint = bodyRectangle.x+(bodyRectangle.width/2)
+	
+		int midpoint = bodyRectangle.x+(bodyRectangle.width/2);
 
+		if(midpoint != -1){
 			if(midpoint>=340){
-				bit1.off();
-				bit2.on();
+				rightGPIO.on();
+				leftGPIO.off();
 			}else if(midpoint <= 300){
-				bit1.on();
-				bit2.off();
+				rightGPIO.off();
+				leftGPIO.on();
 			}else{
-				bit1.on();
-				bit2.on();
+				rightGPIO.on();
+				leftGPIO.on();
 			}
 		}
+
 
 		// future<bool> imaLedja = async(launch::async, 
 		// 							[&]{return isPresent(BodyCascade,
@@ -75,23 +77,5 @@ int main(int argc, char *argv[]){
 		// 							[&]{return isPresent(FaceCascade,
 		// 							 frame,
 		// 							  faceRectangle);});
-	// // 	if(imaLica.get()){
-	// // 		cout<<"Face detected"<<'\n';
-	// // 		//bit1.off();
-	// // 		//bit2.off();
-	// // 		//send message through GPIO pins to stop the motors
-	// // 	}
-	// // 	else if(imaLedja.get()){
-	// // 		cout<<backRectangle.x+(backRectangle.width/2);
-	// // 		//bit1.on();
-	// // 		//bit2.on();
-	// // 		//send message to follow depending on where is the body
-	// // 	}
-	// // 	else{
-	// // 		cout<<"Nothing detected"<<'\n';
-	// // 		//bit1.off();
-	// // 		//bit2.off();
-	// // 		//send message to stop through GPIO pins
-	// // 	}
-	// // }
+
 }}
--- projectCompile.sh
+++ projectCompile.sh
@@ -1,2 +1,2 @@
-g++ main.cpp ./include/FaceBodyRecognition.cpp -o reckoner `pkg-config --cflags --libs opencv4` -pthread
+g++ main.cpp ./include/FaceBodyRecognition.cpp -o reckoner `pkg-config --cflags --libs opencv4` -pthread -lcppgpio
 exit 0
